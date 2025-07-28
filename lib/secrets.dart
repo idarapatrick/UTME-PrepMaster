774930f1c911b1fe293726998b82ec22d7d53014
LassyGAKUBA <l.gakuba1@alustudent.com>
2025-07-28 17:28:14 +0200

    removed secrets.dart file for security

--- lib/secrets.dart
+++
@@ -1,3 +0,0 @@
-// Gemini API Key
-
-const String geminiAPIKey = 'AIzaSyCK08CTbb0pz_3IcLpKSxAypolD70_j9hA';
--- lib/secrets.example.dart
+++
@@ -1,4 +0,0 @@
-// example of how to create your own API keys to test the AI tutor service
-// add this file to your project and rename it to secrets.dart
-
-const String geminiAPIKey = 'your_api_key_here';
